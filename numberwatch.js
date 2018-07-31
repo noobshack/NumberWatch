e698b9b025e1fad06b99832413665a2faf407099
BrentonW <bwolcott@kzoinnovations.com>
2018-07-31 18:48:56 -0400

    Overwatch Number Crunch Creation
    The start of getting the numbers from the api
    
    and allowing users to enter in their own number.

---
+++ numberwatch.js
@@ -0,0 +1,23 @@
+var crunchNumbers = function () {
+
+	var battletag = document.getElementById('battletag').value
+	var region = document.getElementById('region').value
+	var platform = document.getElementById('platform').value
+
+	var url = "https://ow-api.com/v1/stats/" + platform + "/" + region + "/" + battletag + "/profile";
+
+	var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("demo").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", url, true);
+  xhttp.send();
+
+	var input1 = document.getElementById('input1').value;
+	var input2 = document.getElementById('input2').value;
+
+	console.log(battletag, region, platform);
+
+}
